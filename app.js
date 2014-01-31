6643aee10e911ded174450fbd65e3783830cfe6e
Grant Miiller <grant@m2iller.com>
2014-01-30 21:07:57 -0500

    Parses and builds urls, so 'google' is a valid argument

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var url = 'httpfxzghauihiusds://:google';
+var url = 'google';
 
 if(url.search(/^(ftp|http|https):\/\//) === -1) {
     console.log('Invalid protocol, cleaning up and assuming http');
-    url = url.replace(/^.*:\/\//, 'http://');
+    url = 'http://' + url.replace(/^.*:\/\//, '');
     console.log(url);
 }
 
 if(url.search(/[\w]+\./) === -1) {
-    console.log('No valid thing');
-} else {
-    console.log('valid thing');
+    console.log('No valid thing end. Assuming \'com\'');
+    url += '.com';
 }
+
+console.log(url);
